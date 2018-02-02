402f1cbdfb74ede9df029fb1e86995d8c326f505
Fabián Camp <fabian.camp@yahoo.com>
2018-02-01 23:59:13 -0600

    Cambios hechos a la clase principal del juego y nueva clase de Menu

--- core/src/mx/itesm/jgj/JudafaalsGreatJourney.java
+++ core/src/mx/itesm/jgj/JudafaalsGreatJourney.java
@@ -1,16 +1,19 @@
 package mx.itesm.jgj;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class JudafaalsGreatJourney extends ApplicationAdapter {
+public class JudafaalsGreatJourney extends Game {
 
 	@Override
 	public void create () {
 
+		// Pone Pantalla inicial.
+		setScreen(new MenuJudafaals(this)); // Solo objetos de Game pueden correr este comando
 	}
 
 
---
+++ core/src/mx/itesm/jgj/MenuJudafaals.java
@@ -0,0 +1,91 @@
+package mx.itesm.jgj;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+
+class MenuJudafaals implements Screen {
+
+    // Variables de instancia
+    private JudafaalsGreatJourney jdj;
+
+    // Camara
+    private OrthographicCamera camara;
+    private Viewport vista;
+
+    // Dimensiones del mundo
+    public static final float ANCHO = 1280;
+    public static final float ALTO = 720;
+
+    // Batch
+    private SpriteBatch batch;
+
+    public MenuJudafaals(JudafaalsGreatJourney judafaalsGreatJourney) {
+
+        this.jdj = judafaalsGreatJourney;
+    }
+
+    // Método que se encarga de... (preguntarle al profesor).
+    @Override
+    public void show() {
+
+        crearCamara();
+        crearMenu();
+        batch = new SpriteBatch();
+    }
+
+    private void crearMenu() {
+        //todo
+
+    }
+
+    // Método que se encarga de crear a la cámara a usar.
+    private void crearCamara() {
+
+        camara = new OrthographicCamera(ANCHO, ALTO);
+        camara.position.set(ANCHO/2,ALTO/2, 0);
+        camara.update();
+        vista = new StretchViewport(ANCHO, ALTO, camara);
+    }
+
+    @Override
+    public void render(float delta) {
+
+        Gdx.gl.glClearColor(1,1,1,1); // Borra la pantalla con el color blanco en RGB.
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.setProjectionMatrix(camara.combined); // Escala los objetos.
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+        vista.update(width, height);
+    }
+
+    @Override
+    public void pause() {
+        //todo
+    }
+
+    @Override
+    public void resume() {
+        //todo
+    }
+
+    @Override
+    public void hide() {
+        //todo
+    }
+
+    @Override
+    public void dispose() {
+        //todo
+    }
+}
