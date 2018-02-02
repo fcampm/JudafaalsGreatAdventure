9020699e39c486f4a3aa0788c76fd0f02fb447d1
Fabián Camp <fabian.camp@yahoo.com>
2018-02-01 23:14:44 -0600

    Initial commit

---
+++ desktop/src/mx/itesm/jgj/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package mx.itesm.jgj.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import mx.itesm.jgj.JudafaalsGreatJourney;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new JudafaalsGreatJourney(), config);
+	}
+}
