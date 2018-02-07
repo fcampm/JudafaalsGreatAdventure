ad4a24551d34851f1bce1d189731b19cf27c3cec
Fabián Camp <fabian.camp@yahoo.com>
2018-02-07 14:33:33 -0600

    Creación de la clase PantallaAbout.java y ya está lista la transición entre esas dos pantallas.

--- core/src/mx/itesm/jgj/MenuJudafaals.java
+++ core/src/mx/itesm/jgj/MenuJudafaals.java
@@ -4,7 +4,14 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -32,6 +39,9 @@ class MenuJudafaals implements Screen {
     // Batch
     private SpriteBatch batch;
 
+    // Escenas
+    private Stage escenaMenu;
+
     public MenuJudafaals(JudafaalsGreatAdventure judafaalsGreatAdventure) {
 
         this.jdj = judafaalsGreatAdventure;
@@ -48,7 +58,26 @@ class MenuJudafaals implements Screen {
 
     private void crearMenu() {
 
+        escenaMenu = new Stage(vista);
 
+        TextureRegionDrawable trdPlay = new TextureRegionDrawable(new TextureRegion(new Texture("play.png")));
+        TextureRegionDrawable trdOnClick = new TextureRegionDrawable(new TextureRegion(new Texture("playOnClick.png")));
+
+        ImageButton btnPlay = new ImageButton(trdPlay, trdOnClick);
+        btnPlay.setPosition(ANCHO/2 - btnPlay.getWidth()/2, ALTO/2 - btnPlay.getHeight()/2); //Centramos el botón en la pantalla.
+
+        btnPlay.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+                Gdx.app.log("ClickListener","Hizo click el usuario");
+                // Cambia de pantalla, solo lo puede hacer 'juego' una escena no.
+                jdj.setScreen(new PantallaAbout(jdj));
+            }
+        }); // Click y touch son equivalentes.
+
+        escenaMenu.addActor(btnPlay);
+        Gdx.input.setInputProcessor(escenaMenu);
     }
 
     // Método que se encarga de crear a la cámara a usar.
@@ -67,6 +96,8 @@ class MenuJudafaals implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         batch.setProjectionMatrix(camara.combined); // Escala los objetos.
+
+        escenaMenu.draw();
 
     }
 
